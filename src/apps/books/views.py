130cae33b40df6679e78378ac2447821471536d1
berdibaevich <berdibaevich.me@gmail.com>
2026-07-21 23:03:41 +0500

    feat: create books list api using simple api_view decorators

--- src/apps/books/views.py
+++ src/apps/books/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import status, response
 
-# Create your views here.
+from .models import Category, Book
+
+
+@api_view(['GET'])
+def books(request):
+    if request.method == "GET":
+        books = Book.objects.values('id', 'title', 'description')
+        return response.Response(data=books, status=status.HTTP_200_OK)
