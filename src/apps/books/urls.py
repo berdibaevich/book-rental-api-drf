1c2ebc0a8aca147ee628f60c65e260f18a10124d
berdibaevich <berdibaevich.me@gmail.com>
2026-07-22 23:48:07 +0500

    feat: create PUT api for update book

--- src/apps/books/urls.py
+++ src/apps/books/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.books, name='book-list'),
-    path('/create', views.create_book, name='book-create'),
+    path('create/', views.create_book, name='book-create'),
+    path('<int:pk>/put/', views.put_book, name='book-put'),
+
 
 ]
--- src/apps/books/views.py
+++ src/apps/books/views.py
@@ -54,3 +54,50 @@ def create_book(request):
     new_book.categories.set(category_ids)
 
     return response.Response({'id': new_book.id, 'title': new_book.title}, status=status.HTTP_201_CREATED)
+
+
+@api_view(['PUT'])
+def put_book(request, pk):
+    try:
+        book = Book.objects.get(pk = pk)
+    except Book.DoesNotExist:
+        return response.Response(
+            {'errors': 'Book not found.'},
+            status=status.HTTP_404_NOT_FOUND
+        )
+    
+    data = request.data
+    errors = {}
+
+    if 'title' not in data or data.get('title') is None or not str(data.get('title')).strip():
+        errors['title'] = ['This field cannot be null or empty.']
+
+    if 'description' not in data:
+        errors['description'] = ['This field is required.']
+    
+    if 'category_ids' not in data or not isinstance(data.get('category_ids'), list):
+        errors['category_ids'] = ['This field is required and must be a list of IDs.']
+
+    if errors:
+        return response.Response({'errors': errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    category_ids = data.get('category_ids')
+    existing_categories = []
+
+    if category_ids:
+        existing_categories = Category.objects.filter(id__in=category_ids)
+        if len(existing_categories) != len(set(category_ids)):
+            return response.Response(
+                {'errors': 'Some category IDs dont exist.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+
+    book.title = data['title']
+    book.description = data['description']
+    book.save()
+
+    book.categories.set(existing_categories)
+
+    return response.Response({'message': 'Book updated successfully.'}, status=status.HTTP_200_OK)
