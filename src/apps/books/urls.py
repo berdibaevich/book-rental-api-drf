1a26ff328807e5438a20d020358f35c76168c483
berdibaevich <berdibaevich.me@gmail.com>
2026-07-23 21:54:16 +0500

    feat: add detail, update and delete for book

--- src/apps/books/urls.py
+++ src/apps/books/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.book_list_create, name='book-list-create'),
+    path('<int:pk>/', views.book_detail_update_delete, name='book-detail'),
 ]
--- src/apps/books/views.py
+++ src/apps/books/views.py
@@ -52,3 +52,102 @@ def book_list_create(request):
         new_book.categories.set(category_ids)
 
         return response.Response({'id': new_book.id, 'title': new_book.title}, status=status.HTTP_201_CREATED)
+
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def book_detail_update_delete(request, pk):
+    try:
+        book = Book.objects.get(pk = pk)
+    except Book.DoesNotExist:
+        return response.Response(
+            {'errors': 'Book not found.'},
+            status=status.HTTP_404_NOT_FOUND
+        )
+    
+    data = request.data
+    errors = {}
+
+    # 1. GET (Detail)
+    if request.method == 'GET':
+        return response.Response(
+        {
+            'id': book.id,
+            'title': book.title,
+            'description': book.description,
+        },
+        status=status.HTTP_200_OK,
+    )
+
+
+    elif request.method == 'PUT':
+        if 'title' not in data or data.get('title') is None or not str(data.get('title')).strip():
+            errors['title'] = ['This field cannot be null or empty.']
+
+        if 'description' not in data:
+            errors['description'] = ['This field is required.']
+        
+        if 'category_ids' not in data or not isinstance(data.get('category_ids'), list):
+            errors['category_ids'] = ['This field is required and must be a list of IDs.']
+
+        if errors:
+            return response.Response({'errors': errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        category_ids = data.get('category_ids')
+        existing_categories = []
+
+        if category_ids:
+            existing_categories = Category.objects.filter(id__in=category_ids)
+            if len(existing_categories) != len(set(category_ids)):
+                return response.Response(
+                    {'errors': 'Some category IDs dont exist.'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            
+
+        book.title = data['title']
+        book.description = data['description']
+        book.save()
+
+        book.categories.set(existing_categories)
+
+        return response.Response({'message': 'Book updated successfully.'}, status=status.HTTP_200_OK)
+
+
+    elif request.method == 'PATCH':
+        if 'title' in data:
+            if (title_val := data.get('title')) is None or not str(title_val).strip():
+                errors['title'] = ['This field cannot be null or empty!']   
+            else:
+                book.title = title_val
+
+        if 'description' in data:
+            book.description = data.get('description')
+
+        if 'category_ids' in data:
+            if not isinstance(category_ids := data.get('category_ids'), list):
+                errors['category_ids'] = ['category_ids must be a list of integers.']
+            
+            elif category_ids:
+                existing_categories = Category.objects.filter(id__in = category_ids)
+                if len(existing_categories) != len(set(category_ids)):
+                    errors['category_ids'] = ['Some category IDs dont exist.']
+                else:
+                    book.categories.set(category_ids)
+            else:
+                book.categories.clear()
+        
+        if errors:
+            return response.Response({'errors': errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        book.save()
+
+        return response.Response(
+            {'message': 'Book updated successfully [PATCH].'},
+            status=status.HTTP_200_OK
+        )
+
+    elif request.method == 'DELETE':
+        book.delete()
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
