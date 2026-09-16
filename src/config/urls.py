e37620b002ed2f7d7c4bc507f5af657d7b7b0587
berdibaevich <berdibaevich.me@gmail.com>
2026-09-16 15:43:38 +0500

    refactor: update api path

--- src/config/urls.py
+++ src/config/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/books/', include('apps.books.urls'))
+    path('api/', include('apps.books.urls'))
 ]
